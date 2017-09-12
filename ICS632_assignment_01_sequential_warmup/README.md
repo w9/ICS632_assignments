3ae3d963dbc0258448517e5707c9612d8a0f9636
Xun Zhu <zhuxun2@gmail.com>
2017-09-12 12:46:45 -1000

    commit

--- ICS632_assignment_01_sequential_warmup/README.md
+++ ICS632_assignment_01_sequential_warmup/README.md
@@ -12,11 +12,11 @@ Note that in the tiled version, I used four separate for-loops for the edge
 cases when N is not divisible by the block size, as opposed to having one
 for-loop and branching using if-statements, which would be slower.
 
-With $N=18,000$ and using the Intel Compiler (`icc`), the running time for i-j was
-about 2 seconds. However, with the array being of type double (8 bytes = 64
-bits), this means the the memory allocated for either arrays would be
-$$64 \times 18,000^2 \approx 4.83 \times 2^{32} \text{bits},$$
-which necessitates the `-mcmodel=medium` parameter when compiled.
+With $N=18,000$ and using the Intel Compiler (`icc`), the running time for i-j
+was about 2 seconds. However, with the array being of type double (8 bytes = 64
+bits), this means the the memory allocated for either arrays would be $$64
+\times 18,000^2 \approx 4.83 \times 2^{32} \text{bits},$$ which necessitates the
+`-mcmodel=medium` parameter when compiled.
 
 An interesting observation is that `icc` (14.0.2.144 Build 20140120) did
 not generate faster code than `gcc` (7.1.0). The binary created by the latter
@@ -24,6 +24,26 @@ finished in 1.99 seconds.
 
 I wrote a Python script for invoking the compiler with various parameter
 combinations, collecting the results, and aggregate the results into a CSV file.
+
+I noticed that the first few runs take much longer than following runs, usually
+over 8 minutes or so. I suppose this is when the OS is recovering from the last
+task which potentially tapped into swap, but I'm not so sure. For example, here
+are the logs of the first few runs in a particular experiement, notice how the
+wall clock time dropped from hundreds of seconds to about two seconds:
+
+  algo  bs  wall_time     rep l1_load_misses llc_load_misses
+  ----  --  ---------     --- -------------- ---------------
+  ij    1   194.70775351  1   4719950260     168155701
+  ji    1   589.410447918 1   11118078659    317424545
+  tiled 512 212.40171788  1   5067632874     169008293
+  tiled 1   11.366876506  1   645914378      47143981
+  tiled 2   1.969204341   1   451714702      42381861
+  tiled 3   1.965555544   1   451693187      42362063
+  tiled 4   1.992775057   1   451775277      42408239
+  tiled 5   1.99024135    1   456029172      45395570
+
+To compensate for that, I decided to do a couple of *warm-up* runs before the
+real runs.
 
 Question 2
 ==========
--- ICS632_assignment_01_sequential_warmup/compile_run_aggregate.py
+++ ICS632_assignment_01_sequential_warmup/compile_run_aggregate.py
@@ -26,7 +26,8 @@ ALGO_CODES = {
 
 
 def compile_run_and_perf(rep, algo='tiled', bs=1):
-    cmd = '{COMPILER} ./main.c -O3 -o main -DN={N} -DBS={BS} -DALGO={ALGO} -mcmodel=medium'.format(COMPILER=COMPILER, N=N, BS=bs, ALGO=ALGO_CODES[algo])
+    cmd = '{COMPILER} ./main.c -O3 -o main -DN={N} -DBS={BS} -DALGO={ALGO} -mcmodel=medium'.format(
+        COMPILER=COMPILER, N=N, BS=bs, ALGO=ALGO_CODES[algo])
     print(cmd)
     cmdp = run(cmd.split(' '), stdout=PIPE, stderr=PIPE)
     print('Stdout: {}'.format(cmdp.stdout))
@@ -39,18 +40,35 @@ def compile_run_and_perf(rep, algo='tiled', bs=1):
     # print('Stderr: {}'.format(cmdp.stderr))
 
     return {
-        'algo': algo,
-        'rep': rep,
-        'bs': bs,
-        'wall_time': float(RE_WALL_TIME.findall(cmdp.stderr.decode('utf-8'))[0]),
-        'l1_load_misses': int(RE_L1_LOAD_MISSES.findall(cmdp.stderr.decode('utf-8'))[0].replace(',', '')),
-        'llc_load_misses': int(RE_LLC_LOAD_MISSES.findall(cmdp.stderr.decode('utf-8'))[0].replace(',', '')),
+        'algo':
+        algo,
+        'rep':
+        rep,
+        'bs':
+        bs,
+        'wall_time':
+        float(RE_WALL_TIME.findall(cmdp.stderr.decode('utf-8'))[0]),
+        'l1_load_misses':
+        int(
+            RE_L1_LOAD_MISSES.findall(cmdp.stderr.decode('utf-8'))[0].replace(
+                ',', '')),
+        'llc_load_misses':
+        int(
+            RE_LLC_LOAD_MISSES.findall(cmdp.stderr.decode('utf-8'))[0].replace(
+                ',', '')),
     }
 
 
 def main():
     wall_times = []
-    for rep in [x+1 for x in range(N_REP)]:
+
+    # Warm-up runs
+    for rep in range(3):
+        wall_times.append(compile_run_and_perf(0, 'ij'))
+        wall_times.append(compile_run_and_perf(0, 'ji'))
+        wall_times.append(compile_run_and_perf(0, 'tiled', 4))
+
+    for rep in [x + 1 for x in range(N_REP)]:
         print('============== rep = {} =============='.format(rep))
 
         print('---------- ij ----------')
@@ -59,13 +77,16 @@ def main():
         print('---------- ji ----------')
         wall_times.append(compile_run_and_perf(rep, 'ji'))
 
-        bs_candidates = set([x+1 for x in range(N_SEQUENTIAL)] + [round(2**(x/2)) for x in range(8,40)])
+        bs_candidates = sorted(
+            list(
+                set([x + 1 for x in range(N_SEQUENTIAL)] +
+                    [round(2**(x / 2)) for x in range(8, 40)])))
         for bs in bs_candidates:
             print('---------- tiled (bs = {}) ----------'.format(bs))
             wall_times.append(compile_run_and_perf(rep, 'tiled', bs))
 
     with open('results_{}.csv'.format(environ['HOSTNAME']), 'w') as f:
-        f.write('# HOSTNAME = {}'.format(environ['HOSTNAME']))
+        f.write('# HOSTNAME = {}\n'.format(environ['HOSTNAME']))
         wr = csv.DictWriter(f, wall_times[0].keys())
         wr.writeheader()
         wr.writerows(wall_times)

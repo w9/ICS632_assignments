b65a780137801005cc008b674b591928886bad8e
Xun Zhu <zhuxun2@gmail.com>
2017-09-11 18:10:42 -1000

    commit

--- ICS632_assignment_01_sequential_warmup/slurm.bash
+++ ICS632_assignment_01_sequential_warmup/slurm.bash
@@ -3,7 +3,7 @@
 #SBATCH -n 1                    # Number of tasks
 #SBATCH -c 10                   # Number of cores requested
 #SBATCH -N 1                    # Number of nodes requested
-#SBATCH -t 30                   # Runtime in minutes
+#SBATCH -t 240                  # Runtime in minutes
 #SBATCH -o stdout               # Standard out goes to this file
 #SBATCH -e stderr               # Standard err goes to this file
 #SBATCH --mail-type ALL
